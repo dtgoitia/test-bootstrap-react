a824c45dfa35a14ca1cc75a4d5b7d28bfd04b722
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-10-10 14:00:13 +0100

    KK

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {MyButton} from './components/MyButton.js';
 import './App.css';
 
-class App extends Component {
+const buttonArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I"
+];
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          buttonArray.map((b, i) => {
+            return <MyButton key={i} b={b} />;
+          })
+        }
       </div>
     );
   }
---
+++ src/components/MyButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+// const kk = c => {
+//   console.log('this:', c);
+//   console.log('this.props.children:', c.props);
+// };
+
+class MyButton extends React.Component {
+  render() {
+    const b = this.props.b;
+    return (
+      <Button
+        bsSize="large"
+        onClick={b.func}
+      >
+        {b.name}
+      </Button>
+    )
+  }
+}
+
+module.exports = {
+  MyButton: MyButton
+};
